6d3be38f35e3b6081cb817e22d7ace24199d72e5
ryanking13 <def6488@gmail.com>
2024-06-17 20:12:43 +0900

    Fix parametrize

--- test/test_wheel_utils.py
+++ test/test_wheel_utils.py
@@ -4,8 +4,7 @@ from auditwheel_emscripten.wheel_utils import is_emscripten_wheel
 
 
 @pytest.mark.parametrize(
-    "wheel_name",
-    "expected",
+    "wheel_name, expected",
     [
         ("foo-1.0-cp38-cp38-linux_x86_64.whl", False),
         ("foo-1.0-py3-none-any.whl", False),
