e3eb2f6cd39f7f496f16de9e4f77166f3f204185
ryanking13 <def6488@gmail.com>
2024-06-17 20:11:13 +0900

    Fix typo

--- test/test_wheel_utils.py
+++ test/test_wheel_utils.py
@@ -9,9 +9,8 @@ from auditwheel_emscripten.wheel_utils import is_emscripten_wheel
     [
         ("foo-1.0-cp38-cp38-linux_x86_64.whl", False),
         ("foo-1.0-py3-none-any.whl", False),
-        ("foo-1.0-cp312-cp312-pyodide_2024_0_wasm32.whl")(
-            "foo-1.0-cp312-cp312-emscripten_3_1_52_wasm32.whl"
-        ),
+        ("foo-1.0-cp312-cp312-pyodide_2024_0_wasm32.whl", True),
+        ("foo-1.0-cp312-cp312-emscripten_3_1_52_wasm32.whl", True),
     ],
 )
 def test_is_emscripten_wheel(wheel_name, expected):
