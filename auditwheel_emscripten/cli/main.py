f1b4af1275dec991e65c9f628eed0df5675953d6
Sangheon Lee <comp.evenharder@gmail.com>
2025-10-12 19:24:52 +0900

    Replace typer with click

--- auditwheel_emscripten/cli/main.py
+++ auditwheel_emscripten/cli/main.py
@@ -1,16 +1,14 @@
 from pathlib import Path
 
-import typer
+import click
 
 from .. import function_type, get_exports, get_imports, repair, show
 from ..show import locate_dependency
 from ..wheel_utils import unpack_if_wheel
 
-app = typer.Typer()
 
-
-@app.callback(no_args_is_help=True)
-def main():
+@click.group(no_args_is_help=True)
+def app():
     """Auditwheel-like tool for emscripten wheels and shared libraries."""
 
 
@@ -43,19 +41,18 @@ def print_dylib(
 
 
 @app.command("show")
+@click.argument("wheel_or_so_file", type=click.Path(path_type=Path))
+@click.option("--with-runtime-paths", "-r", is_flag=True, help="Show runtime paths.")
 def _show(
-    wheel_or_so_file: Path = typer.Argument(
-        ..., help="Path to wheel or a shared library file."
-    ),
-    show_runtime_paths: bool = typer.Option(
-        False,
-        "-r",
-        "--with-runtime-paths",
-        help="Show runtime paths.",
-    ),
+    wheel_or_so_file: Path,
+    show_runtime_paths: bool,
 ):
     """
     Show shared library dependencies of a wheel or a shared library file.
+
+    \b
+    Arguments:
+        WHEEL_OR_SO_FILE: Path to wheel or a shared library file. (required)
     """
     try:
         libraries = show(wheel_or_so_file)
@@ -68,25 +65,33 @@ def _show(
 
 
 @app.command("repair")
+@click.argument("wheel_file", type=click.Path(path_type=Path))
+@click.option(
+    "--libdir",
+    type=click.Path(path_type=Path),
+    default="lib",
+    help="Path to the directory containing the shared libraries.",
+    show_default=True,
+)
+@click.option(
+    "--output-dir",
+    type=click.Path(path_type=Path),
+    default=None,
+    help="Directory to output repaired wheel or shared library. (default: overwrite the input file)",
+)
+@click.option("--with-runtime-paths", "-r", is_flag=True, help="Show runtime paths.")
 def _repair(
-    wheel_file: Path = typer.Argument(..., help="Path to wheel file."),
-    libdir: Path = typer.Option(
-        "lib",
-        help="Path to the directory containing the shared libraries.",
-    ),
-    output_dir: Path = typer.Option(
-        None,
-        help="Directory to output repaired wheel or shared library. (default: overwrite the input file)",
-    ),
-    show_runtime_paths: bool = typer.Option(
-        False,
-        "-r",
-        "--with-runtime-paths",
-        help="Show runtime paths.",
-    ),
+    wheel_file: Path,
+    libdir: Path,
+    output_dir: Path | None,
+    show_runtime_paths: bool,
 ):
     """
     Repair a wheel file: copy shared libraries to the wheel directory.
+
+    \b
+    Arguments:
+        WHEEL_FILE: Path to wheel file. (required)
     """
     try:
         repaired_wheel = repair(
@@ -105,35 +110,54 @@ def _repair(
 
 
 @app.command("copy")
+@click.argument("wheel_file", type=click.Path(path_type=Path))
+@click.option(
+    "--libdir",
+    type=click.Path(path_type=Path),
+    default="lib",
+    help="Path to the directory containing the shared libraries.",
+    show_default=True,
+)
+@click.option(
+    "--output-dir",
+    type=click.Path(path_type=Path),
+    default=None,
+    help="Directory to output repaired wheel or shared library. (default: overwrite the input file)",
+)
 def _copy(
-    wheel_file: Path = typer.Argument(..., help="Path to wheel file."),
-    libdir: Path = typer.Option(
-        "lib",
-        help="Path to the directory containing the shared libraries.",
-    ),
-    output_dir: Path = typer.Option(
-        None,
-        help="Directory to output repaired wheel or shared library. (default: overwrite the input file)",
-    ),
+    wheel_file: Path,
+    libdir: Path,
+    output_dir: Path | None,
 ):
     """
     [Deprecated] Copy shared libraries to the wheel directory. Works same as `repair`. Use `repair` instead.
+
+    \b
+    Arguments:
+        WHEEL_FILE: Path to wheel file. (required)
     """
     return _repair(wheel_file, libdir, output_dir)
 
 
 @app.command("exports")
+@click.argument("wheel_or_so_file", type=click.Path(path_type=Path))
+@click.option(
+    "--show-type",
+    is_flag=True,
+    help="Show function type.",
+    default=False,
+    show_default=True,
+)
 def _exports(
-    wheel_or_so_file: Path = typer.Argument(
-        ..., help="Path to wheel or a shared library file."
-    ),
-    show_type: bool = typer.Option(
-        False,
-        help="Show function type.",
-    ),
+    wheel_or_so_file: Path,
+    show_type: bool,
 ):
     """
     Show exports of a wheel or a shared library file.
+
+    \b
+    Arguments:
+        WHEEL_OR_SO_FILE: Path to wheel or a shared library file. (required)
     """
     try:
         exports = get_exports(wheel_or_so_file)
@@ -154,17 +178,24 @@ def _exports(
 
 
 @app.command("imports")
+@click.argument("wheel_or_so_file", type=click.Path(path_type=Path))
+@click.option(
+    "--show-type",
+    is_flag=True,
+    help="Show function type.",
+    default=False,
+    show_default=True,
+)
 def _imports(
-    wheel_or_so_file: Path = typer.Argument(
-        ..., help="Path to wheel or a shared library file."
-    ),
-    show_type: bool = typer.Option(
-        False,
-        help="Show function type.",
-    ),
+    wheel_or_so_file: Path,
+    show_type: bool,
 ):
     """
     Show imports of a wheel or a shared library file.
+
+    \b
+    Arguments:
+        WHEEL_OR_SO_FILE: Path to wheel or a shared library file. (required)
     """
     try:
         imports = get_imports(wheel_or_so_file)
